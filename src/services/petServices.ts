4fc9ea4a226a7e8de4a4f4789545c51fcb73c679
prikshit-singh <prikshitlatherlather@gmail.com>
2025-05-26 22:55:37 +0530

    api url

--- src/services/petServices.ts
+++ src/services/petServices.ts
@@ -1,6 +1,6 @@
 
 import Cookies from 'js-cookie';
-const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? 'http://gitgurus.com:4000/api';
+const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? 'https://gitgurus.com/api';
 
 export const createPet = async (payload: any) => {
   const token = Cookies.get('token');
